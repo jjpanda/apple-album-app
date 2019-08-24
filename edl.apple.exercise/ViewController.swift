6f7aae294715011f8f7bebbf3664ff81748596de
phua.jj <phua.jj@pg.com>
2019-08-25 00:26:31 +0800

    Initial Commit

---
+++ edl.apple.exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  edl.apple.exercise
+//
+//  Created by jjpan on 25/8/19.
+//  Copyright © 2019 jjpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
